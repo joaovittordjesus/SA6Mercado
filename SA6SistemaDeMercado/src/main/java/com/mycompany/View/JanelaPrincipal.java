17a45da7284e5832bf96ef6e331e083a7b456488
vitalinoleo <leonardosilvadelira@gmail.com>
2023-12-11 17:02:16 -0300

    ProjetoMercado

--- SA6SistemaDeMercado/src/main/java/com/mycompany/View/JanelaPrincipal.java
+++ SA6SistemaDeMercado/src/main/java/com/mycompany/View/JanelaPrincipal.java
@@ -4,17 +4,21 @@
  */
 package com.mycompany.View;
 
+import java.awt.Component;
+
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
 public class JanelaPrincipal extends JFrame {
     private JTabbedPane jTPane;
+    private Component tab2;
 
     public JanelaPrincipal() {
         jTPane = new JTabbedPane();
         add(jTPane);
         ProdutoPainel tab1 = new ProdutoPainel();
         jTPane.add("Produtos", tab1);
+        jTPane.add("Cliente", tab2);
         setBounds(100, 100, 600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
